24a2e2111c779f16976770d8838055b5bdf05680
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-06-25 00:30:14 +0400

    Update 0001_Variable names.java

--- 0001_Variable names.java
+++ 0001_Variable names.java
@@ -1,1 +1,47 @@
+1.
+cost_j = costOfJumperInStore
+//стоимость джемпера в магазине
 
+2.
+marketJumper = costOfJumperInMarket
+//стоимость джемпера на рынке
+
+3.
+citiesFly = listOfCitiesForAirTravel
+//список городов для авиаперелётов
+
+4.
+sumInRub = sumSalariesInRubOfAllEmployees
+//сумма зарплат всех сотрудников в рублях
+
+5.
+countOfbook = countOfBooksInLibraryInLeninskyDistrict
+//количество книг в библиотеке в ленинском районе
+
+6.
+keybilet = keyTicketNumberForHistoryExam
+//ключ-номер билета на экзамене по истории
+
+7.
+tiketQuestion = valueQuestionInTicketForHistoryExam
+//значение-вопрос в билете на экзамене по истории
+
+8.
+t_Msk = timeByMoscowTime
+//время по Москве
+
+9.
+indexFilms = indexOfFilmInCollectionFilms
+//индекс фильма в массиве коллекции фильмов
+
+10.
+weapon_1 = meleeWeapon
+//оружие ближнего боя
+
+11.
+musicCount = countOfMusicTracksInTheAlbum
+//количество музыкальных треков в массиве альбома
+
+12.
+water = mlOfWaterInMeasuringCup
+//количество миллилитров воды в мерном стакане
