d41631c5115485e57679b0262b2e903cb9cd483e
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-06-26 02:00:12 +0400

    Update 0001_VariableNames_02.java

--- 0001_VariableNames_02.java
+++ 0001_VariableNames_02.java
@@ -1,24 +1,24 @@
 6.1
--
+- resultOfOperation = resultOf
 -
 -
 -
 -
   
 6.2
--
--
--
--
+- arrayListOfHuman
+- mapOfBooksAuthorAndTitle
+- factorialOfNumber
+- convertedIntegerToShort
   
 6.3
--
+- resultOfSubtraction, resultOfAddition  в контексте арифметических операций
 -
 -
   
 6.4
--
--
--
--
--
+- gor = horizontalSizeOfField
+- ver = verticalSizeOfField
+- readCnsl = readCoordinateFromConsole
+- gorCrd = horizontalCoordinate
+- var = varForTrafficLightsOperation
