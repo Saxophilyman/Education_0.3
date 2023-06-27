9a48e5f8e3d3b94a4aaa0dd0bac257bd13d34f63
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-06-27 17:45:34 +0400

    Update 0001_VariableNames_03.java

--- 0001_VariableNames_03.java
+++ 0001_VariableNames_03.java
@@ -1,21 +1,25 @@
 7.1
-- 
-- 
--
--
--
+- flagCorrect = isCorrect
+- flagUndo = isUndoOperation
+- human = isHuman
+- notReady = isReady
+- del = isDeleted
+
+  - global = whiteWalkersIsIdentified
 
 7.2
--
--
+- notCorrect = isError
+- right = isFound
 -
 
 7.3
--
+- Имена пременных  вроде i в циклах в основном делаю "полными" в стиле index, indexOf(сущность), теперь буду с лёгким сердцем писать просто i.
+  Думаю в случае обхода двумерных массивов удобнее обозначить indexColumn/indexLines, так как можно легко запутаться
 
 7.4
--
--
--
+- fireOn/fireOff
+- Ascending/Descending
+- Check/UnCheck
 
 7.5
+Далеко не всем временным переменным выданы номальные имена, иногда может быть просто какая-то буква. bolean b, bolean true (чисто как проходные)
