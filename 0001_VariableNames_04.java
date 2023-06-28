43be5b917fca5ea09ec23fc4a4d059936dfa0618
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-06-28 15:57:55 +0400

    Update 0001_VariableNames_04.java

--- 0001_VariableNames_04.java
+++ 0001_VariableNames_04.java
@@ -1,1 +1,20 @@
-1. 
+1. countOfGoalsInFirstTime / countOfGoalsInSecondTime = countInFirstTimeGoals / countInSecondTimeGoals
+
+
+
+
+
+
+
+ 5. имена со скрытым, подразумеваемым ("очевидным" только самому кодировщику) смыслом
+ global = 
+ подразумевалось, что переменная просто взята "от балды" для себя и она и так понятна (мне) как общий результат true/false найденных ходоков
+
+ 6. из серии спецификаторов (префиксов и постфиксов)
+ maxSpeed = speedMax
+ minSpeed = speedMin
+  
+
+ 7. из серии спецификаторов суффиксов
+ square = square_km
+ countOfWaters = countOfWaters_liters
