a05c345b7eeb3b459cd080785453acffd960ad60
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-06-28 17:20:35 +0400

    Update 0001_VariableNames_04.java

--- 0001_VariableNames_04.java
+++ 0001_VariableNames_04.java
@@ -1,20 +1,40 @@
-1. countOfGoalsInFirstTime / countOfGoalsInSecondTime = countInFirstTimeGoals / countInSecondTimeGoals
+1. Избегайте имён, имеющих разный смысл, но похожие имена
+ 
+    01. countOfGoalsInFirstTime / countOfGoalsInSecondTime = inFirstTimeGoalsCount / inSecondTimeGoalsCount
 
+     
+2. Избегайте имён, включающих цифры, а также имен, содержащих символы, которые можно спутать с другими символами
+     
+    02.  String1Line = firstStringInLine
+     
 
+3. Избегайте имён стандартных типов, классов, методов и функций
 
+    03. sum = sumOfNumbers
+    04. max = maxNumberInArray
 
+     
+ 4. неинформативные имёна
 
+    05. 
+    06. stringName = nameOfStudent
 
-
+     
  5. имена со скрытым, подразумеваемым ("очевидным" только самому кодировщику) смыслом
- global = 
- подразумевалось, что переменная просто взята "от балды" для себя и она и так понятна (мне) как общий результат true/false найденных ходоков
+ 
+    07. global = allWhiteWalkersIsIdentified
+    впишу в данный пункт, т.к. подразумевалось, что переменная просто взята "от балды" для себя
+    и она и так понятна (мне) как общий результат true/false найденных ходоков
 
+ 
  6. из серии спецификаторов (префиксов и постфиксов)
- maxSpeed = speedMax
- minSpeed = speedMin
-  
+ 
+    08. playerList = rosterOfPlayers
+    09. maxSpeed = speedMax
+    10. minSpeed = speedMin
 
+ 
  7. из серии спецификаторов суффиксов
- square = square_km
- countOfWaters = countOfWaters_liters
+ 
+    11. square = square_km
+    12. countOfWaters = countOfWaters_liters
