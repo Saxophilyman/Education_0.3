d71e5beb629010228b6f81a54cf329b386e649e0
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-06-29 01:49:28 +0400

    Update 0002_ClassNames_01.java

--- 0002_ClassNames_01.java
+++ 0002_ClassNames_01.java
@@ -1,1 +1,23 @@
+1. Имена классов
+
+  01. Working = Work
+  02. pizza_Ubizza = Pizzeria 
+  03. poezd = Train
+  04. makingPizza = FoodMaker
+  05. 
+
+
+2. Одно слово для каждой концепции
+  
+  01.
+  02
+  03.
+  04.
+  05.
+  06.
+  07.
+
+  
+спортзал
+поезд
 
