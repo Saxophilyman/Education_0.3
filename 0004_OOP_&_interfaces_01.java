551db722a41e700064f00198a9555183983d8a7d
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-07-03 17:42:51 +0400

    Update 0004_OOP_&_interfaces_01.java

--- 0004_OOP_&_interfaces_01.java
+++ 0004_OOP_&_interfaces_01.java
@@ -1,1 +1,8 @@
+Пока что достаточно слабо знаком с методом фабрики и интерфейсами, напишу насколько понимаю
 
+1.  FabricCars fabricCars = FabricCars.collectTypeOfCars(audi);
+2.  Pizza pizza = Pizza.makeTypeOfPyzza(cheezePizza);
+3.  Soldiers soldiers = Soldiers.createUnitTypeOfSoldiers(infantry);
+
+Соответственно если брать по аналогии, интерфейс по сборке машин/пиццы лучше назвать 
+  FabricCarsImp / PizzaConstructorImp
