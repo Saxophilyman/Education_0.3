1df20f1c5be67c598c9649083fff7e409911b96b
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-07-07 17:29:44 +0400

    Update 0007_Variables_&_Their_Values_01.java

--- 0007_Variables_&_Their_Values_01.java
+++ 0007_Variables_&_Their_Values_01.java
@@ -1,11 +1,33 @@
-    01.
+
+    01. Завершил работу с переменной по окончании выполнения блока кода,
+        присвоив ей минимальное значение типа integer
+        varForTrafficLightsOperation = Integer.MIN_VALUE;
       
-    02.
+    02. В методе инициализация переменной int sumOfDiscount = 0; из начала блока кода перенесена непосредственно к месту её использования
+        if (price.length < 3) {
+            return 0;
+        }
+        Arrays.sort(price);
+        int sumOfDiscount = 0;
+        for (int i = price.length-3; i >= 0; i -= 3) {
+            sumOfDiscount += price[i];
+        }
+        return sumOfDiscount;
       
-    03.
+    03. Перенесено объявление переменной int year = 0; из начала блока кода перенесена непосредственно к месту её использования
+        Integer[][] yearNumTree = new Integer[H][W];
+        String[] resultTree = new String[H];
+        for (int index = 0; index < tree.length; index++) {
+            //код
+        }
+        int year = 0;
+        while (year < N) {
+            //код
+        }    
       
-    04.
-      
+    04. Также завершена работа с переменной по окончании выполнения блока кода, с присвоением "недопустимого" значения -1
+        year = -1;
+
     05.
       
     06.
