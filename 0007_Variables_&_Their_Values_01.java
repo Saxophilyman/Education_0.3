6d6fe7ef0ba034821bc5944d85cd1326ffa54a84
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-07-08 17:34:11 +0400

    Update 0007_Variables_&_Their_Values_01.java

--- 0007_Variables_&_Their_Values_01.java
+++ 0007_Variables_&_Their_Values_01.java
@@ -14,7 +14,7 @@
         }
         return sumOfDiscount;
       
-    03. Перенесено объявление переменной int year = 0; из начала блока кода перенесена непосредственно к месту её использования
+    03. Перенесено объявление переменной int year = 0; из начала блока кода непосредственно к месту её использования
         Integer[][] yearNumTree = new Integer[H][W];
         String[] resultTree = new String[H];
         for (int index = 0; index < tree.length; index++) {
@@ -33,7 +33,7 @@
           final int NUMBER_NINE_IN_ASCII = 57;
           final int SYMBOL_WHITE_WALKERS_IN_ASCII = 61;
 
-    06. Перенесено объявление переменной StringBuilder s = new StringBuilder(); из начала блока кода перенесена непосредственно к месту её использования
+    06. Перенесено объявление переменной StringBuilder s = new StringBuilder(); из начала блока кода непосредственно к месту её использования
         public static String Keymaker(int k) {        
             int[] doors = new int[k];
             for (int step = 1; step <= k; step++) {
@@ -64,11 +64,50 @@
         //код
         }
 
-    08. 
+    08. метод считывает введённые в консоль числа текстом для определения координат
+        применён assert для проверки на предмет допустимости их значений != null
+        public void readCoordinateFromConsole(Player player) {
+        String readCrossZeroCord = null;
+        try {
+            readCrossZeroCord = reader.readLine();
+        } catch (Exception e) {
+            System.out.println("Что-то пошло не так, извините");
+        }
+        assert readCrossZeroCord != null;
+        //много кода
+    }
       
-    09.
-      
-    10.
+    09. Перенесены объявления разных типов хранилищ из начала блока кода непосредственно к месту её использования
+        Set brands = new HashSet<>();
+        brands.add("Nike");
+        brands.add("IBM");
+        brands.add("Google");
+        brands.add("Apple");
+        brands.forEach((e) -> { System.out.println(e); });
+
+        int[] nums = { 3, 4, 2, 1, 6, 7 };
+        Arrays.stream(nums).sorted().forEach((e) -> { System.out.println(e); });
+
+        int[] inums = { 3, 5, 6, 7, 5 };
+        IntConsumer icons = i -> System.out.print(i + " ");
+        Arrays.stream(inums).forEach(icons);
+
+        System.out.println();
+        long[] lnums = { 13L, 3L, 6L, 1L, 8L };
+        LongConsumer lcons = l -> System.out.print(l + " ");
+        Arrays.stream(lnums).forEach(lcons);
+
+    10. Перенесены объявления переменных text1 и text2 из начала блока кода непосредственно к месту их использования
+        BufferedImage img = ImageIO.read(new File(s));
+        Graphics g = img.getGraphics();
+        int w = 150, h = 25;
+        g.setColor(Color.RED); // задать цвет прямоугольника
+        g.fillRect((img.getWidth() - w) / 2, (img.getHeight() - h) / 2, w, h);  // нарисовать прямоугольник
+        g.setColor(Color.black);
+        String text1 = "Hello,";
+        String text2 = "World!";
+        int widthText1 = g.getFontMetrics().stringWidth(text1);
+        int widthText2 = g.getFontMetrics().stringWidth(text2);
       
     11.
       
