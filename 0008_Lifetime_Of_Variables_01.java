6299ba9fd8158dd54bff2160eac22edf253b5d1b
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-07-10 21:54:30 +0400

    Update 0008_Lifetime_Of_Variables_01.java

--- 0008_Lifetime_Of_Variables_01.java
+++ 0008_Lifetime_Of_Variables_01.java
@@ -1,6 +1,49 @@
-    01.
-    02.
-    03.
+    01. Уменьшение области видимости arr до цикла do ... while:
+        
+    before:
+        int[] array = {0};
+        do {  
+            array = modifyNumber(array); // сохраняет результаты в другом месте
+        } while (array.length < 100);
+
+    after:
+        {
+        int[] array = {0};
+        do {  
+            array = modifyNumber(array); 
+        } while (array.length < 100);
+        }
+
+
+    02. Уменьшение области видимости переменных random / generatedNumber до завершения цикла fori:
+    before:
+        Random random = new Random();
+        int generatedNumber;
+        int[] arrayOfTelemetric = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            generatedNumber = random.nextInt(256) + 1;
+            //код
+        }
+        //дальнейший код
+
+
+    after:
+        int[] arrayOfTelemetric = new int[arrayLength];
+        {
+        Random random = new Random();
+        int generatedNumber;
+        for (int i = 0; i < arrayLength; i++) {
+            generatedNumber = random.nextInt(256) + 1;
+            //код
+        }
+        }
+        //дальнейший код
+
+        
+    03. Уменьшение области видимости переменных random / generatedNumber до завершения цикла fori:
+
+
+        
     04.
     05.
     06.
